d3f45b51f970bc57835fe4a445bed65bae242f9b
SFInterlenghi <stinterlenghi@gmail.com>
2026-03-23 08:02:08 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="ISI-Tool Home", layout="wide")
-
-st.title("Welcome to the ISI-Tool Web App")
-st.markdown("""
-This is the new Python-based economic evaluation tool.
-Please use the sidebar on the left to navigate to the **Input Data** page to begin configuring your chemical plant scenarios.
-""")
